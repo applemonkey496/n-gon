4a42fcf65098c7067db375c5d70a7fd754c5d506
applemonkey496 <55333787+applemonkey496@users.noreply.github.com>
2020-06-06 13:12:55 -0700

    add some comments

--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,14 @@
 
+// called once
 function setup() {
   createCanvas(550, 550);
-  angleMode(RADIANS);
+  angleMode(RADIANS); // so the equations work
 
-  const n = window.prompt('How many sides? (Integer)');
+  const n = window.prompt('How many sides? (Integer)'); // get input
 
-  const r = Math.min(width, height) / 2 - 50;
+  const r = Math.min(width, height) / 2 - 50; // circumradius for the shape
 
+  // get arrays for the x- and y- coordinates
   const x = getPoints({
     sides: n,
     radius: r,
@@ -20,6 +22,7 @@ function setup() {
     mode: 'y'
   });
 
+  // draw the shape
   for (let i = 0; i < n; i++) {
     const x1 = x[i];
     const y1 = y[i];
@@ -31,12 +34,16 @@ function setup() {
     line(x1, y1, x2, y2);
   }
 
+  // formulae for side length and exterior angles
   const ext = 360 / n;
   const side = 2 * r * sin(PI / n);
 
+  // display result of formulae
   text(`Turn ${round(ext, 1)}°, forward ${round(side, 2)}px`, 25, 25);
 }
 
+// function to get the x / y points of an n-gon
+// see https://gamedev.stackexchange.com/questions/29559/algorithm-for-constructing-the-corners-of-a-regular-n-sided-polygon
 function getPoints(data = {
   sides: 3,
   radius: 100,
@@ -63,6 +70,12 @@ function getPoints(data = {
   return points;
 }
 
+/* 
+ * A rounding function so I can specify the number of decimal places
+ *
+ * Number.EPSILON accounts for the some minor discrepancies with floating point numbers
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/EPSILON 
+ */
 function round(number, places) {
   const exp = Math.pow(10, places);
   return rounded = Math.round((number + Number.EPSILON) * exp) / exp;
